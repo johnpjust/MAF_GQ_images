ee468ecbfd598edeaec65621eb0984609eeab748
johnpjust <johnpjust@gmail.com>
2019-11-23 10:39:53 -0600

    updates

--- main.py
+++ main.py
@@ -10,8 +10,39 @@ import gzip
 import functools
 import random
 from skimage import io, transform
+import os
+import datetime
+import pathlib
+import json
 
 tf.random.set_random_seed(None)
+
+
+def img_heatmap(model, sess, imgcre, args):
+    # rand_box = np.append(tf.cast(tf.multiply(tf.cast(imgcre.shape[:2], tf.float32),tf.constant(0.1)), tf.int32).numpy(), [3])
+    rows = imgcre.shape[0]-args.rand_box[0]
+    cols = imgcre.shape[1] - args.rand_box[1]
+    heatmap = np.zeros((np.int(rows/args.spacing), np.int(cols/args.spacing)))
+    with tf.device(args.device):
+        if type(args.vh) is not np.ndarray:
+            im_breakup_array = np.zeros((np.int(cols / args.spacing), args.rand_box_size * args.rand_box_size * 3), dtype=np.float32)
+            for i in range(np.int(rows/args.spacing)*args.spacing):
+                if not i%args.spacing:
+                    for j in range(np.int(cols/args.spacing)*args.spacing):
+                        if not j%args.spacing:
+                            # im_breakup_array[np.int(j/args.spacing),:] = tf.reshape(tf.image.crop_to_bounding_box(imgcre, i, j, args.rand_box_size, args.rand_box_size)/128 - 1, [-1]).numpy()
+                            im_breakup_array[np.int(j/args.spacing),:] = (imgcre[i:(i+args.rand_box_size), j:(j+args.rand_box_size)]/128 - 1).reshape([-1])
+                    heatmap[np.int(i/args.spacing), :] = np.squeeze(model.eval(im_breakup_array, sess))
+        else:
+            for i in range(np.int(rows/args.spacing)*args.spacing):
+                im_breakup_array = np.zeros((np.int(cols / args.spacing), args.vh.shape[1]), dtype=np.float32)
+                if not i%args.spacing:
+                    for j in range(np.int(cols/args.spacing)*args.spacing):
+                        if not j%args.spacing:
+                            # im_breakup_array[np.int(j/args.spacing),:] = tf.squeeze(tf.matmul(tf.reshape(tf.image.crop_to_bounding_box(imgcre, i, j, args.rand_box_size, args.rand_box_size)/128 - 1, [1, -1]), args.vh)).numpy()
+                            im_breakup_array[np.int(j/args.spacing),:] = np.squeeze(np.matmul((imgcre[i:(i+args.rand_box_size), j:(j+args.rand_box_size)]/128 - 1).reshape([1,-1]), args.vh))
+                    heatmap[np.int(i/args.spacing), :] = np.squeeze(model.eval(im_breakup_array, sess))
+    return heatmap
 
 def img_preprocessing(imgcre, args):
     # rand_box = np.append(tf.cast(tf.multiply(tf.cast(imgcre.shape[:2], tf.float32),tf.constant(0.1)), tf.int32).numpy(), [3])
@@ -84,6 +115,7 @@ def load_dataset(args):
             cliplist.append(sess.run(dataset_train))
         svdmat = np.vstack(cliplist)
         _, _, args.vh = scipy.linalg.svd(svdmat, full_matrices=False)
+        # args.vh = args.vh[:, :args.svdnum]
         img_preprocessing_train = functools.partial(img_preprocessing, args=args)
         img_preprocessing_val = functools.partial(img_preprocessing, args=args)
         dataset_train = tf.data.Dataset.from_tensor_slices(train_data.astype(np.float32))  # .float().to(args.device)
@@ -111,13 +143,15 @@ args.max_iterations = 20000
 args.num_layers = 5
 args.num_hidden = [100]
 args.act = tf.nn.relu
-args.vh = 0 #0 =no, 1=yes
+args.vh = 1 #0 =no, 1=yes
 args.prefetch_size = 1  # data pipeline prefetch buffer size
 args.parallel = 16  # data pipeline parallel processes
 args.img_size = 0.25;  ## resize img between 0 and 1
 args.preserve_aspect_ratio = True;  ##when resizing
 args.rand_box_init = 0.1  ##relative size of random box from image
 args.manualSeed = None
+args.device = '/gpu:0'  # '/gpu:0'
+# args.svdnum = 100
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
@@ -151,36 +185,61 @@ sess.run(init)
 t.train(sess, train_data, val, cont_data, early_stopping=args.early_stopping, check_every_N=args.check_every, show_log=args.show_log, max_iterations=args.max_iterations, saver_name='temp/tmp_model')
 # t.train(sess, data.astype(np.float32), val_data=val.astype(np.float32), early_stopping=100, check_every_N=5, show_log=True, batch_size=100, max_iterations=20000, saver_name='temp/tmp_model')
 
-# import matplotlib.pyplot as plt
-# import scipy.stats
-s = model.gen(sess, 5000)
-# out = model.eval(train_data, sess)
-out2 = model.eval(sess.run(val), sess)
-out3 = model.eval(sess.run(cont_data), sess)
-sout_ = model.eval(s,sess)
-# dist = scipy.stats.johnsonsu.fit(out)
-# out = (np.arcsinh((out - dist[-2]) / dist[-1]) * dist[1] + dist[0])
-# out2 = (np.arcsinh((out2 - dist[-2]) / dist[-1]) * dist[1] + dist[0])
-# out3 = (np.arcsinh((out3 - dist[-2]) / dist[-1]) * dist[1] + dist[0])
-# sout = (np.arcsinh((sout_ - dist[-2]) / dist[-1]) * dist[1] + dist[0])
+# # import matplotlib.pyplot as plt
+# # import scipy.stats
+# s = model.gen(sess, 5000)
+# # out = model.eval(train_data, sess)
+# out2 = model.eval(sess.run(val), sess)
+# out3 = model.eval(sess.run(cont_data), sess)
+# sout_ = model.eval(s,sess)
+# # dist = scipy.stats.johnsonsu.fit(out)
+# # out = (np.arcsinh((out - dist[-2]) / dist[-1]) * dist[1] + dist[0])
+# # out2 = (np.arcsinh((out2 - dist[-2]) / dist[-1]) * dist[1] + dist[0])
+# # out3 = (np.arcsinh((out3 - dist[-2]) / dist[-1]) * dist[1] + dist[0])
+# # sout = (np.arcsinh((sout_ - dist[-2]) / dist[-1]) * dist[1] + dist[0])
 
-# plt.figure()
-# plt.hist(out, 50, density=True, alpha=0.3, label='cifar_train')
-# plt.hist(out2, 50, density=True, alpha=0.3, label='cifar_val')
-# plt.hist(out3, 50, density=True, alpha=0.3, label='svhn')
-# plt.hist(sout, 50, density=True, alpha=0.3, label='samples')
-# plt.xlabel('MAF Density')
-# plt.legend()
-# plt.xlim([-6,3])
-# plt.savefig(r'C:\Users\justjo\Desktop\maf_cifarVSsvhn_density_samples_'
-#             r'.png', bbox_inches='tight')
-#
-# saver = tf.train.Saver()
-# saver.save(sess, r'C:\Users\justjo\PycharmProjects\maf_tf\Models\maf_digitsVSfashion_h100_f5_tanh_dim784\model')
-#
-# # s = model.gen(sess, 5000)
-# plt.figure();plt.scatter(data[:,0], data[:,1], alpha=1, label='data')
-# plt.scatter(test[:,0], test[:,1], alpha=0.2)
-# plt.scatter(s[:,0], s[:,1], alpha=0.3, label='sampled')
-# st = np.matmul(s, vh)
-# plt.scatter(st[:,0], st[:,1], alpha=0.2, label='MAF_SVD')
+args.spacing = 2
+
+args.path = os.path.join(r'D:\pycharm_projects\GQC_images_tensorboard', 'MAF_layers{}_h{}_vh{}_resize{}_boxsize{}_{}'.format(
+    args.num_layers, args.num_hidden, args.vh, args.img_size, args.rand_box_init,
+    str(datetime.datetime.now())[:-7].replace(' ', '-').replace(':', '-')))
+
+print('Creating directory experiment..')
+pathlib.Path(args.path).mkdir(parents=True, exist_ok=True)
+with open(os.path.join(args.path, 'args.json'), 'w') as f:
+    json.dump(str(args.__dict__), f, indent=4, sort_keys=True)
+
+print('Loading dataset..')
+trainval = glob.glob(r'D:\GQC_Images\GQ_Images\Corn_2017_2018/*.png')
+train_data = np.vstack([np.expand_dims(img_load(x, args), axis=0) for x in trainval])
+cont_data = glob.glob(r'D:\GQC_Images\GQ_Images\test_images_broken/*.png')
+cont_data = np.vstack([np.expand_dims(img_load(x, args), axis=0) for x in cont_data])
+
+## heatmaps:  change args.spacing closer to 1 to get more spatial resolution on the heatmap
+heat_map_train = []
+heat_map_train.extend(img_heatmap(model, sess, img, args) for img in train_data)
+heatmap_t = np.array(heat_map_train)
+heat_map_cont = []
+heat_map_cont.extend(img_heatmap(model, sess, img, args) for img in cont_data)
+heatmap_c = np.array(heat_map_cont)
+
+## summaries
+mean_t = np.mean(heatmap_t.reshape((heatmap_t.shape[0], -1)), axis=1)
+std_t = np.std(heatmap_t.reshape((heatmap_t.shape[0], -1)), axis=1)
+min_t = np.min(heatmap_t.reshape((heatmap_t.shape[0], -1)), axis=1)
+max_t = np.max(heatmap_t.reshape((heatmap_t.shape[0], -1)), axis=1)
+
+mean_c = np.mean(heatmap_c.reshape((heatmap_c.shape[0], -1)), axis=1)
+std_c = np.std(heatmap_c.reshape((heatmap_c.shape[0], -1)), axis=1)
+min_c = np.min(heatmap_c.reshape((heatmap_c.shape[0], -1)), axis=1)
+max_c = np.max(heatmap_c.reshape((heatmap_c.shape[0], -1)), axis=1)
+
+totals_t = np.stack([mean_t, std_t, min_t, max_t]).T
+totals_c = np.stack([mean_c, std_c, min_c, max_c]).T
+
+np.savetxt(args.path + '/totals_t' + '_' + str(args.spacing) + '_' + str(heatmap_t.reshape((heatmap_t.shape[0], -1)).shape[1]) + '.csv', totals_t, delimiter=',')
+np.savetxt(args.path + '/totals_c' + '_' + str(args.spacing) + '_' + str(heatmap_c.reshape((heatmap_c.shape[0], -1)).shape[1]) + '.csv', totals_c, delimiter=',')
+
+## heatmap arrays
+np.save(args.path + '/totals_t' + '_' + str(args.spacing) + '_' + str(heatmap_t.reshape((heatmap_t.shape[0], -1)).shape[1]) + '.npy', heatmap_t)
+np.save(args.path + '/totals_c' + '_' + str(args.spacing) + '_' + str(heatmap_c.reshape((heatmap_c.shape[0], -1)).shape[1]) + '.npy', heatmap_c)
